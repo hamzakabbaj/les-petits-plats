3ee4079f5c626ac4e3eb76265a7dd9fa245cee4d
Hamza Kabbaj <hamza.kabbaj@live.fr>
2025-04-23 17:42:04 +0100

    Update FilterSelectBox functionality to toggle search box visibility; adjust styles in CSS and SCSS for improved layout and usability of filter elements.

--- js/templates/FilterSelectBox.js
+++ js/templates/FilterSelectBox.js
@@ -22,6 +22,87 @@ class FilterSelectBox {
       </ul>
     `;
     $wrapper.innerHTML = filterSelectBox;
+
+    const $title = $wrapper.querySelector(
+      ".main__header__filters__select-box__title"
+    );
+    $title.addEventListener("click", () => {
+      if ($title.classList.contains("opened")) {
+        this.closeFilterSelectBox();
+      } else {
+        this.openFilterSelectBox();
+      }
+    });
     return $wrapper;
+  }
+
+  getTitleElement(all = false) {
+    if (all) {
+      return document.querySelectorAll(
+        `.main__header__filters__select-box__title`
+      );
+    }
+    return document.querySelector(
+      `#${this._filter.id} .main__header__filters__select-box__title`
+    );
+  }
+
+  getSearchBoxElement(all = false) {
+    if (all) {
+      return document.querySelectorAll(
+        `.main__header__filters__select-box__search`
+      );
+    }
+    return document.querySelector(
+      `#${this._filter.id} .main__header__filters__select-box__search`
+    );
+  }
+
+  getListElement(all = false) {
+    if (all) {
+      return document.querySelectorAll(
+        `.main__header__filters__select-box__list`
+      );
+    }
+    return document.querySelector(
+      `#${this._filter.id} .main__header__filters__select-box__list`
+    );
+  }
+
+  openFilterSelectBox() {
+    this.closeAllFilterSelectBox();
+    const $search = this.getSearchBoxElement();
+    const $list = this.getListElement();
+    const $title = this.getTitleElement();
+    $title.classList.add("opened");
+    $title.style.borderRadius = "11px 11px 0 0";
+    $search.style.display = "block";
+    $list.style.display = "flex";
+  }
+
+  closeFilterSelectBox() {
+    const $search = this.getSearchBoxElement();
+    const $list = this.getListElement();
+    const $title = this.getTitleElement();
+    $title.classList.remove("opened");
+    $title.style.borderRadius = "11px";
+    $search.style.display = "none";
+    $list.style.display = "none";
+  }
+
+  closeAllFilterSelectBox() {
+    const $titles = this.getTitleElement(true);
+    $titles.forEach((title) => {
+      title.classList.remove("opened");
+      title.style.borderRadius = "11px";
+    });
+    const $search = this.getSearchBoxElement(true);
+    $search.forEach((search) => {
+      search.style.display = "none";
+    });
+    const $list = this.getListElement(true);
+    $list.forEach((list) => {
+      list.style.display = "none";
+    });
   }
 }
