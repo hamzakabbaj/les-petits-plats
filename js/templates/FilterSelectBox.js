61fcbc85ba79670ce42a813ede413ca0eba40bf7
Hamza Kabbaj <hamza.kabbaj@live.fr>
2025-04-24 07:52:06 +0100

    Refactor FilterSelectBox to improve event handling and item selection; streamline methods for managing search input and item display, enhancing overall functionality and maintainability.

--- js/templates/FilterSelectBox.js
+++ js/templates/FilterSelectBox.js
@@ -4,6 +4,8 @@ class FilterSelectBox {
     this._selected_items = [];
   }
 
+  // ------------------ ELEMENTS ------------------------
+
   createFilterSelectBox() {
     const $wrapper = document.createElement("div");
     $wrapper.classList.add("main__header__filters__select-box");
@@ -22,74 +24,8 @@ class FilterSelectBox {
     $wrapper.innerHTML = filterSelectBox;
     $wrapper.appendChild(this.createFilterSelectBoxItems(this._filter.list));
 
-    const $title = $wrapper.querySelector(
-      ".main__header__filters__select-box__title"
-    );
+    this.addEventsListeners($wrapper);
 
-    const $clear_icon = $wrapper.querySelector(
-      ".main__header__filters__select-box__search__clear"
-    );
-
-    const $search_input = $wrapper.querySelector(
-      ".main__header__filters__select-box__search input"
-    );
-
-    const $search_items = $wrapper.querySelectorAll(
-      ".main__header__filters__select-box__list li"
-    );
-
-    $search_items.forEach((item) => {
-      item.addEventListener("click", (e) => {
-        // If doesn't have the class selected, add it
-        if (!item.classList.contains("selected")) {
-          item.classList.add("selected");
-          this._selected_items.push(item.textContent);
-        }
-      });
-    });
-
-    $search_input.addEventListener("input", () => {
-      const search_value = $search_input.value;
-      if (search_value.length > 0) {
-        $clear_icon.classList.remove("hidden");
-      } else {
-        $clear_icon.classList.add("hidden");
-      }
-
-      // Contains the items
-      const filtered_list = this._filter.list.filter((item) =>
-        item.toLowerCase().includes(search_value.toLowerCase())
-      );
-      // Replace the items
-      const $list = $wrapper.querySelector(
-        ".main__header__filters__select-box__list"
-      );
-      $list.innerHTML = filtered_list
-        .map((item) => `<li>${item}</li>`)
-        .join("");
-    });
-
-    $clear_icon.addEventListener("click", () => {
-      $search_input.value = "";
-      // Focus on the search input
-      $search_input.focus();
-      $clear_icon.classList.add("hidden");
-    });
-
-    $title.addEventListener("click", () => {
-      if ($title.classList.contains("opened")) {
-        this.closeFilterSelectBox();
-      } else {
-        this.openFilterSelectBox();
-      }
-    });
-
-    const $body = document.querySelector("body");
-    $body.addEventListener("click", (e) => {
-      if (!e.target.closest(".main__header__filters__select-box")) {
-        this.closeAllFilterSelectBox();
-      }
-    });
     return $wrapper;
   }
 
@@ -110,39 +46,83 @@ class FilterSelectBox {
     return $wrapper;
   }
 
-  getTitleElement(all = false) {
-    if (all) {
-      return document.querySelectorAll(
-        `.main__header__filters__select-box__title`
-      );
-    }
-    return document.querySelector(
-      `#${this._filter.id} .main__header__filters__select-box__title`
-    );
+  // ------------------- EVENTS -------------------------
+  addEventsListeners($wrapper) {
+    const $title = this.getTitleElement($wrapper);
+    const $clear_icon = this.getSearchBoxClearIconElement($wrapper);
+    const $search_input = this.getSearchBoxInputElement($wrapper);
+    const $search_items = this.getSearchItemsElements($wrapper);
+    const $body = document.querySelector("body");
+
+    console.log($search_items);
+
+    this.addItemsClickListener($search_items);
+    this.addSearchInputListener($search_input, $clear_icon, $wrapper);
+    this.addClearIconClickListener($clear_icon, $search_input);
+    this.addChevronIconClickListener($title);
+    this.addBodyClickListener($body);
   }
 
-  getSearchBoxElement(all = false) {
-    if (all) {
-      return document.querySelectorAll(
-        `.main__header__filters__select-box__search`
+  addSearchInputListener($search_input, $clear_icon, $wrapper) {
+    $search_input.addEventListener("input", () => {
+      const search_value = $search_input.value;
+      console.log(search_value);
+      if (search_value.length > 0) {
+        $clear_icon.classList.remove("hidden");
+      } else {
+        $clear_icon.classList.add("hidden");
+      }
+
+      // Contains the items
+      const filtered_list = this._filter.list.filter((item) =>
+        item.toLowerCase().includes(search_value.toLowerCase())
       );
-    }
-    return document.querySelector(
-      `#${this._filter.id} .main__header__filters__select-box__search`
-    );
+      // Replace the items
+      this.setSearchItems(filtered_list, $wrapper);
+    });
   }
 
-  getListElement(all = false) {
-    if (all) {
-      return document.querySelectorAll(
-        `.main__header__filters__select-box__list`
-      );
-    }
-    return document.querySelector(
-      `#${this._filter.id} .main__header__filters__select-box__list`
-    );
+  addItemsClickListener($search_items) {
+    $search_items.forEach((item) => {
+      item.addEventListener("click", (e) => {
+        // If doesn't have the class selected, add it
+        if (!item.classList.contains("selected")) {
+          item.classList.add("selected");
+          this._selected_items.push(item.textContent);
+        }
+      });
+    });
   }
 
+  addClearIconClickListener($clear_icon, $search_input, $wrapper) {
+    $clear_icon.addEventListener("click", () => {
+      $search_input.value = "";
+      // Focus on the search input
+      $search_input.focus();
+      $clear_icon.classList.add("hidden");
+      this.setSearchItems(this._filter.list, $wrapper);
+    });
+  }
+
+  addChevronIconClickListener($title) {
+    $title.addEventListener("click", () => {
+      if ($title.classList.contains("opened")) {
+        this.closeFilterSelectBox();
+      } else {
+        this.openFilterSelectBox();
+      }
+    });
+  }
+
+  addBodyClickListener($body) {
+    $body.addEventListener("click", (e) => {
+      if (!e.target.closest(".main__header__filters__select-box")) {
+        this.closeAllFilterSelectBox();
+      }
+    });
+  }
+
+  //----------------------- ACTIONS -----------------------
   changeChevronIcon() {
     const $title = this.getTitleElement();
     $title.querySelector("i").classList.toggle("fa-chevron-down");
@@ -173,20 +153,107 @@ class FilterSelectBox {
   }
 
   closeAllFilterSelectBox() {
-    const $titles = this.getTitleElement(true);
+    const $titles = this.getAllTitleElements();
     $titles.forEach((title) => {
       title.classList.remove("opened");
       title.style.borderRadius = "11px";
       title.querySelector("i").classList.remove("fa-chevron-up");
       title.querySelector("i").classList.add("fa-chevron-down");
     });
-    const $search = this.getSearchBoxElement(true);
+    const $search = this.getAllSearchBoxElements();
     $search.forEach((search) => {
       search.style.display = "none";
     });
-    const $list = this.getListElement(true);
+    const $list = this.getAllListElements();
     $list.forEach((list) => {
       list.style.display = "none";
     });
+  }
+
+  setSearchItems(items, $wrapper) {
+    const $list = this.getListElement($wrapper);
+    $list.innerHTML = items.map((item) => `<li>${item}</li>`).join("");
+  }
+
+  // ------------------- GETTERS ------------------------
+
+  getAllTitleElements() {
+    return document.querySelectorAll(
+      `.main__header__filters__select-box__title`
+    );
+  }
+
+  getTitleElement(wrapper = null) {
+    if (wrapper) {
+      return wrapper.querySelector(`.main__header__filters__select-box__title`);
+    }
+    return document.querySelector(
+      `#${this._filter.id} .main__header__filters__select-box__title`
+    );
+  }
+
+  getAllSearchBoxElements() {
+    return document.querySelectorAll(
+      `.main__header__filters__select-box__search`
+    );
+  }
+
+  getSearchBoxInputElement(wrapper = null) {
+    if (wrapper) {
+      return wrapper.querySelector(
+        `.main__header__filters__select-box__search input`
+      );
+    }
+    return document.querySelector(
+      `#${this._filter.id} .main__header__filters__select-box__search input`
+    );
+  }
+
+  getSearchBoxElement(wrapper = null) {
+    if (wrapper) {
+      return wrapper.querySelector(
+        `.main__header__filters__select-box__search`
+      );
+    }
+    return document.querySelector(
+      `#${this._filter.id} .main__header__filters__select-box__search`
+    );
+  }
+
+  getSearchBoxClearIconElement(wrapper = null) {
+    if (wrapper) {
+      return wrapper.querySelector(
+        `.main__header__filters__select-box__search__clear`
+      );
+    }
+    return document.querySelector(
+      `#${this._filter.id} .main__header__filters__select-box__search__clear`
+    );
+  }
+
+  getAllListElements() {
+    return document.querySelectorAll(
+      `.main__header__filters__select-box__list`
+    );
+  }
+
+  getListElement(wrapper = null) {
+    if (wrapper) {
+      return wrapper.querySelector(`.main__header__filters__select-box__list`);
+    }
+    return document.querySelector(
+      `#${this._filter.id} .main__header__filters__select-box__list`
+    );
+  }
+
+  getSearchItemsElements(wrapper = null) {
+    if (wrapper) {
+      return wrapper.querySelectorAll(
+        `.main__header__filters__select-box__list li`
+      );
+    }
+    return document.querySelectorAll(
+      `#${this._filter.id} .main__header__filters__select-box__list li`
+    );
   }
 }
